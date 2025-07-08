802ef9ddd96071dae1a69c3338f37bdb0e1b33bc
keth <kejsijahaj95@gmail.com>
2025-07-08 18:37:22 +0200

    debounce added

--- app.js
+++ app.js
@@ -7,34 +7,51 @@ let fruitCache = []; // keep data after first fetch
 // get fruits from API
 
 async function getFruits() {
-    if(fruitCache.length) return fruitCache; // already have it
-    const response = await fetch('https://corsproxy.io/?https://www.fruityvice.com/api/fruit/all');
-    if (!response.ok) throw new Error(`HTTP ${response.status}`);
-    fruitCache = await response.json();
-    renderList(fruitCache)
-    return fruitCache;
+  if (fruitCache.length) return fruitCache; // already have it
+  const response = await fetch(
+    "https://corsproxy.io/?https://www.fruityvice.com/api/fruit/all"
+  );
+  if (!response.ok) throw new Error(`HTTP ${response.status}`);
+  fruitCache = await response.json();
+  renderList(fruitCache);
+  return fruitCache;
 }
 
 // display the data
 
 const renderList = (list) => {
-    fruitContainer.innerHTML = "";
+  fruitContainer.innerHTML = "";
 
-    list.forEach(item => {
-        const li = document.createElement("li");
-        li.textContent = item.name;
-        li.className= "card";
-        fruitContainer.appendChild(li);
-    });
-}
+  list.forEach((item) => {
+    const li = document.createElement("li");
+    li.textContent = item.name;
+    li.className = "card";
+    fruitContainer.appendChild(li);
+  });
+};
+
+// filter the data
 
 async function filterSuggestions() {
-    const all = await getFruits();
-    const searchValue = searchInput.value.trim().toLowerCase();
-    const list = searchValue? all.filter(item => item.name.toLowerCase().includes(searchValue)) : all;
+  const all = await getFruits();
+  const searchValue = searchInput.value.trim().toLowerCase();
+  const list = searchValue
+    ? all.filter((item) => item.name.toLowerCase().includes(searchValue))
+    : all;
 
-    renderList(list);
+  renderList(list);
 }
 
-document.addEventListener("DOMContentLoaded", filterSuggestions)
-searchInput.addEventListener("keyup", filterSuggestions);
+// debounce event
+
+function debounce(myFunction, delay = 300) {
+  let time;
+  return (...args) => {
+    // collects any arguments
+    clearTimeout(time);
+    time = setTimeout(() => myFunction.apply(this, args), delay);
+  };
+}
+
+document.addEventListener("DOMContentLoaded", filterSuggestions);
+searchInput.addEventListener("keyup", debounce(filterSuggestions, 300));
